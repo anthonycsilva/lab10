81290feb651144e45068a9386cff9e3793ea9bba
anthonycsilva <anthonycsilva@github.com>
2021-02-09 09:09:42 -0300

    git

--- idle/ex02.py
+++ idle/ex02.py
@@ -17,11 +17,20 @@ def mult(a,b,c):
 def media(a,b,c):
     mostraEntrada(a,b,c)
     return (a+b+c)/3
+def calculaIntervalo(a,b,c):
+    soma = a
+    resultados = list()
+    resultados.append(a)
+    while soma < b:
+        soma = soma + c
+        if soma<b:
+            resultados.append(soma)
+    return sum(resultados)
 def main():
     print('1 - Calculando a Area do Trapézio')
     print('2 - Calculando os Multiplos!')
     print('3 - Calculando a Média')
-    print('4 - Em construção')
+    print('4 - Calculando Soma de Intervalo!')
     print('=-'*20)
 
 
@@ -31,19 +40,31 @@ def main():
     c = int(input('Digite o Terceiro Numero:'))
     print('=-'*20)
 
-    if(a<b):
-        print('Erro')
-    elif opc == 1:
-            print(f'Calculando a Area do Trapézio!')
-            print(calcularTrapezio(a,b,c))
-    elif opc == 2:
-        print(f'Calculando os Multiplos!')
-        print(mult(a,b,c))
-    elif opc == 3:
-        print(f'Calculando a Média')
-        print(media(a,b,c))
+    if(a>b):
+        print('Erro, A é maior que B')
+        return False
+    if( a<0 or b<0 or c<0):
+        print('Erro, Não é permitido Números Negativos')
+        return False
+    if(opc > 4):
+        print('Erro, o programa ainda não tem essa opção')
+        return False
+    else:
+        if opc == 1:
+                print(f'Calculando a Area do Trapézio!')
+                print('Resultado:',calcularTrapezio(a,b,c))
+        elif opc == 2:
+            print(f'Calculando os Multiplos!')
+            print('Resultado:',mult(a,b,c))
+        elif opc == 3:
+            print(f'Calculando a Média')
+            print('Resultado:',media(a,b,c))
+        elif opc == 4:
+            print('Calculando Soma de Intervalo!')
+            print('Resultado:', calculaIntervalo(a,b,c))
+
     
-    return None
+    return True
 
 
 main()
